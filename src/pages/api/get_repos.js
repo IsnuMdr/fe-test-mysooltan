1cda4c5b82145362dc42a54af999d8187f8f2214
isnu.mdr <isnu.mdr@gmail.com>
2023-02-22 12:23:14 +0700

    change response api route get repositories

--- src/pages/api/get_repos.js
+++ src/pages/api/get_repos.js
@@ -11,29 +11,13 @@ export default async (req, res) => {
       auth: process.env.GITHUB_AUTH_TOKEN,
     });
 
-    const followers = await octokit.request(
-      `/users/${user}/followers?per_page=100`
-    );
-    const followerCount = followers.data.length;
-
-    const stars = await octokit.request(`/users/${user}/repos`);
-    const starsCount = stars.data
-      .filter((repo) => !repo.fork)
-      .reduce((acc, item) => {
-        return acc + item.stargazers_count;
-      }, 0);
-
-    const reposStarred = await octokit.request(`/users/${user}/starred`);
-    const starredCount = reposStarred.data.length;
-
+    const users = await octokit.request(`https://api.github.com/${user}`);
     const repoList = await octokit.request(
       `https://api.github.com/users/${user}/repos`
     );
 
     return res.status(200).json({
-      stars: starsCount,
-      followers: followerCount,
-      starred: starredCount,
+      users: users.data,
       repos: repoList.data,
     });
   } catch (error) {
