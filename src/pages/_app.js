7cbb433f04158fd0d6cbad5c1c8cae6220d99fa9
isnu.mdr <isnu.mdr@gmail.com>
2023-02-23 11:29:12 +0700

    add loading progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import "@/styles/globals.css";
+import "nprogress/nprogress.css";
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { apiSlice } from "@/state/apiSlice";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => {
+      nProgress.start();
+    });
+
+    Router.events.on("routeChangeComplete", () => {
+      nProgress.done(false);
+    });
+  }, []);
+
   return (
     <ApiProvider api={apiSlice}>
       <Component {...pageProps} />
